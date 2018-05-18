4fc71d33a6f9015f99a0376f2210db700ca2ff15
michaelakh <michael_ak@hotmail.co.uk>
2018-05-18 19:37:32 +0100

    trying to fix email auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
+    address:              'aspmx.l.google.com',
+    port:                 25,
     user_name:            'emailservices33',
     password:             'secretpassword',
     authentication:       'plain',
