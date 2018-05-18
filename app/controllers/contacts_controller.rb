0ccaa2dcebba908327575b809adf4f783a9d5653
michaelakh <michael_ak@hotmail.co.uk>
2018-05-18 20:04:13 +0100

    take out email because heroku app has no SSL or TSL so add recaptcha to normal page displaying email

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,15 +13,18 @@ class ContactsController < ApplicationController
     @subject = params[:category]
     @question = params[:question]
     
-      if ContactMailer.send_question_email(@user, @subject, @question).deliver
-        flash[:success] = 'Your message has been sent'
+      # When we get SSL or TLS for heroku production server
+      # if ContactMailer.send_question_email(@user, @subject, @question).deliver
         redirect_to email_confirm_path
-      else
-        render contact_path
-      end
   end
   
   def email_confirm
+    if verify_recaptcha
+    else
+      flash[:warning] = 'Please verify that you are not a robot'
+      redirect_to contact_path
+      return
+    end
   end
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-    address:              'aspmx.l.google.com',
-    port:                 25,
+    address:              'smtp.gmail.com',
+    port:                 587,
     user_name:            'emailservices33',
     password:             'secretpassword',
     authentication:       'plain',
