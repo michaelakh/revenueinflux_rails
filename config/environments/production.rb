78f727baa8f735dc4aa4ffe4f3d9756c7b2b949f
michaelakh <michael_ak@hotmail.co.uk>
2018-05-18 19:20:05 +0100

    raise delivery errors on emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "revenueinflux_#{Rails.env}"
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
@@ -77,6 +77,8 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true  }
 
+  
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
